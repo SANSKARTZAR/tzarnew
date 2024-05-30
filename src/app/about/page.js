b81643415f79365859610f37ed016d577e48453a
sanskar choudhari <sanskarchoudhari1122@gmail.com>
2024-05-30 19:49:24 +0530

    gidatdd

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+import ContactForm from "@/app/componants/contact/ContactForm";
+
+export default function about() {
+    return (
+        <main className="flex">
+            <h1>test</h1>
+            <ContactForm />
+        </main>
+    )
+}
