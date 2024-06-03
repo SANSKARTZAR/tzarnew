9778229291a64c89375e41e43982aa4c0e56acf3
sanskar choudhari <sanskarchoudhari1122@gmail.com>
2024-06-03 16:59:30 +0530

    icotest

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,14 @@ import "../assets/styles/mibooz.css";
 import "../assets/styles/mibooz-responsive.css";
 import "../assets/styles/custom.css";
 import "../assets/styles/uicons-regular-rounded.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "../assets/vendors/mibooz-icons/style.css";
+import "../assets/vendors/fontawesome/css/all.min.css";
+import "../assets/vendors/animate/animate.min.css";
+import "../assets/vendors/animate/custom-animate.css";
+import "../assets/vendors/reey-font/stylesheet.css";
+import "../assets/vendors/the-sayinistic-font/stylesheet.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
