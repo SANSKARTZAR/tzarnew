906a022c54c3f00e1c6c0c2fc7557aed46a0d444
sanskar choudhari <sanskarchoudhari1122@gmail.com>
2024-07-02 17:40:23 +0530

    lichange

--- src/app/componants/ShopifyEnquiry/PlanShopify.js
+++ src/app/componants/ShopifyEnquiry/PlanShopify.js
@@ -51,31 +51,31 @@ export default function PlanShopify() {
                                 <p>Advanced E-Commerce Website + Android App</p>
                                 <h2>₹139,000</h2>
                                 <ul>
-                                    <li><span class="S1PPyQ">Shopify Website Creation and Setup</span></li>
-                                    <li><strong><span class="S1PPyQ">Android Hybrid App</span></strong></li>
-                                    <li><strong><span class="S1PPyQ">Google Play Publishing</span></strong></li>
-                                    <li><strong><span class="S1PPyQ">Referral Integration</span></strong></li>
-                                    <li><strong><span class="S1PPyQ">Abandoned Checkout</span></strong></li>
-                                    <li><span class="S1PPyQ">Upto 200 Products Upload By Our Team</span></li>
-                                    <li><span class="S1PPyQ">SEO Ready Website</span></li>
-                                    <li><span class="S1PPyQ">SSL Certificate</span></li></ul>
+                                    <li><span className="S1PPyQ">Shopify Website Creation and Setup</span></li>
+                                    <li><strong><span className="S1PPyQ">Android Hybrid App</span></strong></li>
+                                    <li><strong><span className="S1PPyQ">Google Play Publishing</span></strong></li>
+                                    <li><strong><span className="S1PPyQ">Referral Integration</span></strong></li>
+                                    <li><strong><span className="S1PPyQ">Abandoned Checkout</span></strong></li>
+                                    <li><span className="S1PPyQ">Upto 200 Products Upload By Our Team</span></li>
+                                    <li><span className="S1PPyQ">SEO Ready Website</span></li>
+                                    <li><span className="S1PPyQ">SSL Certificate</span></li></ul>
                                 <ul>
-                                    <li><span class="S1PPyQ">Responsive Website</span></li>
-                                    <li><strong><span class="S1PPyQ">Content 3000 Words</span></strong></li>
-                                    <li><span class="S1PPyQ">Social Media Integration</span></li>
-                                    <li><span class="S1PPyQ">Google Analytics Integration</span></li>
-                                    <li><span class="S1PPyQ">Google Maps Embed</span></li>
-                                    <li><span class="S1PPyQ">Whatsapp Integration</span></li>
-                                    <li><span class="S1PPyQ">High Speed Optimization</span></li>
-                                    <li><span class="S1PPyQ">Search Console Integration</span></li>
-                                    <li><span class="S1PPyQ">Contact Form Integration</span></li>
-                                    <li><span class="S1PPyQ">Custom Favicon</span></li>
-                                    <li><span class="S1PPyQ">Website Policies Drafting</span></li>
-                                    <li><span class="S1PPyQ">Shopify Portal Training Session</span></li>
-                                    <li><span class="S1PPyQ">Payment Gateway Integration</span></li>
-                                    <li><strong><span class="S1PPyQ">Conversion Tracking &amp; Pixel Integration</span></strong></li>
-                                    <li><span class="S1PPyQ">Shipping Gateway Setup &amp; Integration</span></li>
-                                    <li><span class="S1PPyQ">Assistance In Shopify Apps Setup</span></li></ul>
+                                    <li><span className="S1PPyQ">Responsive Website</span></li>
+                                    <li><strong><span className="S1PPyQ">Content 3000 Words</span></strong></li>
+                                    <li><span className="S1PPyQ">Social Media Integration</span></li>
+                                    <li><span className="S1PPyQ">Google Analytics Integration</span></li>
+                                    <li><span className="S1PPyQ">Google Maps Embed</span></li>
+                                    <li><span className="S1PPyQ">Whatsapp Integration</span></li>
+                                    <li><span className="S1PPyQ">High Speed Optimization</span></li>
+                                    <li><span className="S1PPyQ">Search Console Integration</span></li>
+                                    <li><span className="S1PPyQ">Contact Form Integration</span></li>
+                                    <li><span className="S1PPyQ">Custom Favicon</span></li>
+                                    <li><span className="S1PPyQ">Website Policies Drafting</span></li>
+                                    <li><span className="S1PPyQ">Shopify Portal Training Session</span></li>
+                                    <li><span className="S1PPyQ">Payment Gateway Integration</span></li>
+                                    <li><strong><span className="S1PPyQ">Conversion Tracking &amp; Pixel Integration</span></strong></li>
+                                    <li><span className="S1PPyQ">Shipping Gateway Setup &amp; Integration</span></li>
+                                    <li><span className="S1PPyQ">Assistance In Shopify Apps Setup</span></li></ul>
                                 <hr />
                                 <h5>TALK TO US<br />
                                     +91-9972-318-316</h5>
@@ -87,32 +87,32 @@ export default function PlanShopify() {
                                 <p>Advanced E-Commerce Website + Android App + iOS Hybrid App</p>
                                 <h2>₹149,000</h2>
                                 <ul>
-                                    <li><span class="S1PPyQ">Shopify Website Creation and Setup</span></li>
-                                    <li><span class="S1PPyQ">Android Hybrid App</span></li>
-                                    <li><span class="S1PPyQ">Google Play Publishing</span></li>
-                                    <li><strong><span class="S1PPyQ">iOS Web App</span></strong></li>
-                                    <li><span class="S1PPyQ">Referral Integration</span></li>
-                                    <li><span class="S1PPyQ">Abandoned Checkout</span></li>
-                                    <li><span class="S1PPyQ">Upto 200 Products Upload By Our Team</span></li>
-                                    <li><span class="S1PPyQ">SEO Ready Website</span></li></ul>
+                                    <li><span className="S1PPyQ">Shopify Website Creation and Setup</span></li>
+                                    <li><span className="S1PPyQ">Android Hybrid App</span></li>
+                                    <li><span className="S1PPyQ">Google Play Publishing</span></li>
+                                    <li><strong><span className="S1PPyQ">iOS Web App</span></strong></li>
+                                    <li><span className="S1PPyQ">Referral Integration</span></li>
+                                    <li><span className="S1PPyQ">Abandoned Checkout</span></li>
+                                    <li><span className="S1PPyQ">Upto 200 Products Upload By Our Team</span></li>
+                                    <li><span className="S1PPyQ">SEO Ready Website</span></li></ul>
                                 <ul>
-                                    <li><span class="S1PPyQ">SSL Certificate</span></li>
-                                    <li><span class="S1PPyQ">Responsive Website</span></li>
-                                    <li><span class="S1PPyQ">Content 3000 Words</span></li>
-                                    <li><span class="S1PPyQ">Social Media Integration</span></li>
-                                    <li><span class="S1PPyQ">Google Analytics Integration</span></li>
-                                    <li><span class="S1PPyQ">Google Maps Embed</span></li>
-                                    <li><span class="S1PPyQ">Whatsapp Integration</span></li>
-                                    <li><span class="S1PPyQ">High Speed Optimization</span></li>
-                                    <li><span class="S1PPyQ">Search Console Integration</span></li>
-                                    <li><span class="S1PPyQ">Contact Form Integration</span></li>
-                                    <li><span class="S1PPyQ">Custom Favicon</span></li>
-                                    <li><span class="S1PPyQ">Website Policies Drafting</span></li>
-                                    <li><span class="S1PPyQ">Shopify Portal Training Session</span></li>
-                                    <li><span class="S1PPyQ">Payment Gateway Integration</span></li>
-                                    <li><span class="S1PPyQ">Conversion Tracking &amp; Pixel Integration</span></li>
-                                    <li><span class="S1PPyQ">Shipping Gateway Setup &amp; Integration</span></li>
-                                    <li><span class="S1PPyQ">Assistance In Shopify Apps Setup</span></li></ul>
+                                    <li><span className="S1PPyQ">SSL Certificate</span></li>
+                                    <li><span className="S1PPyQ">Responsive Website</span></li>
+                                    <li><span className="S1PPyQ">Content 3000 Words</span></li>
+                                    <li><span className="S1PPyQ">Social Media Integration</span></li>
+                                    <li><span className="S1PPyQ">Google Analytics Integration</span></li>
+                                    <li><span className="S1PPyQ">Google Maps Embed</span></li>
+                                    <li><span className="S1PPyQ">Whatsapp Integration</span></li>
+                                    <li><span className="S1PPyQ">High Speed Optimization</span></li>
+                                    <li><span className="S1PPyQ">Search Console Integration</span></li>
+                                    <li><span className="S1PPyQ">Contact Form Integration</span></li>
+                                    <li><span className="S1PPyQ">Custom Favicon</span></li>
+                                    <li><span className="S1PPyQ">Website Policies Drafting</span></li>
+                                    <li><span className="S1PPyQ">Shopify Portal Training Session</span></li>
+                                    <li><span className="S1PPyQ">Payment Gateway Integration</span></li>
+                                    <li><span className="S1PPyQ">Conversion Tracking &amp; Pixel Integration</span></li>
+                                    <li><span className="S1PPyQ">Shipping Gateway Setup &amp; Integration</span></li>
+                                    <li><span className="S1PPyQ">Assistance In Shopify Apps Setup</span></li></ul>
                                 <hr />
                                 <h5>TALK TO US<br />
                                     +91-9972-318-316</h5>
