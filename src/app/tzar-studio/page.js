d5bab465c73c5719b35bdc01d28ef0a63c9b3c0b
sanskar choudhari <sanskarchoudhari1122@gmail.com>
2024-07-18 18:33:58 +0530

    food shoot

--- src/app/tzar-studio/page.js
+++ src/app/tzar-studio/page.js
@@ -1,22 +1,15 @@
 import React from "react";
-// import PageHeaderTeam from "../components/PageHeader/PageHeaderTeam";
 import BStudioSlider from "../componants/TzarStudio/BStudioSlider";
 import StudioShowcase from "../componants/TzarStudio/StudioShowcase";
 import StudioClients from "../componants/TzarStudio/StudioClients";
-// import StudioShowcase from "../components/TzarStudioPG/StudioShowcase";
-// import StudioClients from "../components/TzarStudioPG/StudioClients";
-// import FoodShoot from "../components/TzarStudioPG/FoodShoot";
-
+import FoodShoot from "../componants/TzarStudio/FoodShoot";
 const Page = () => {
   return (
     <>
-      {/* <PageHeaderTeam  /> */}
       <BStudioSlider page="Tzar Studio" title="Our Studio"/>
       <StudioShowcase/>
       <StudioClients/>
-      {/* 
-      <StudioClients/>
-      <FoodShoot/> */}
+      <FoodShoot/>
     </>
   );
 };
