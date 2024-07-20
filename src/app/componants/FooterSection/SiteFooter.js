f89c5cca443f84181c11ccff0e29899e2a0608be
sanskar choudhari <sanskarchoudhari1122@gmail.com>
2024-07-20 18:13:00 +0530

    footer add

---
+++ src/app/componants/FooterSection/SiteFooter.js
@@ -0,0 +1,212 @@
+/* eslint-disable @next/next/no-img-element */
+import footerData from "@/app/data/footerData";
+import Link from "next/link";
+import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+
+const { bg, title, phone, phone1, phoneIcon, mailIcon, googlepartner, facebook, googlereview, socials, address, city, email, links, year, author } =
+  footerData;
+
+const SiteFooter = () => {
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const formData = new FormData(e.target);
+  //   console.log(formData.get("email"));
+  // };
+
+  return (
+    <footer className="site-footer">
+      <div className="site-footer__top">
+        <div
+          className="site-footer__top-shape"
+          style={{ backgroundImage: `url(${bg.src})` }}
+        ></div>
+        <Container>
+          <div className="site-footer__top-inner">
+            <div className="site-footer__top-left">
+              <h3 className="site-footer__top-left-title">{title}</h3>
+              <a
+                href={`tel:${phone.split(" ").join("")}`}
+                className="site-footer__top-left-phone"
+              >
+                + {phone}
+              </a> &nbsp;&nbsp;
+              <a
+                href={`tel:${phone1.split(" ").join("")}`}
+                className="site-footer__top-left-phone"
+              >
+                + {phone1}
+              </a>
+            </div>
+            <div className="site-footer__top-right">
+              <div className="site-footer__top-right-social">
+                {socials.map(({ id, icon, href }) => (
+                  <a key={id} href={href}>
+                    <i className={icon}></i>
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+
+      <div className="site-footer__middle">
+        <Container>
+          <div className="site-footer__middle-inner">
+            <Row>
+              <Col xl={3} lg={4} className="animated fadeInUp">
+                <div className="footer-widget__column footer-widget__contact">
+                  <h3 className="footer-widget__title">TZAR DIGITAL AGENCY</h3>
+                  <p className="footer-widget__contact-text">
+                    {address} <br /> {city}
+                  </p>
+                  <div className="footer-widget__contact-email-phone">
+                    <h3 className="footer-widget__title">Contact Us</h3>
+                    <div className="footer-flex">
+                      <img src={mailIcon.src} alt="" width="15px" height="15px" /> &nbsp;
+                      <a
+                        href={`mailto:${email}`}
+                        className="footer-widget__contact-text"
+                      >
+                        {email}
+                      </a>
+                    </div>
+                    <img src={phoneIcon.src} alt="" width="15px" /> &nbsp;
+                    <a
+                      href={`tel:${phone.split(" ").join("")}`}
+                      className="footer-widget__contact-text"
+                    >
+                      + {phone}
+                    </a>
+                    <br />
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a
+                      href={`tel:${phone1.split(" ").join("")}`}
+                      className="footer-widget__contact-text"
+                    >
+                      + {phone1}
+                    </a>
+                  </div>
+                </div>
+              </Col>
+              <Col xl={4} lg={4} className="animated fadeInUp">
+                <div className="footer-widget__column footer-widget__links clearfix">
+                  <h3 className="footer-widget__title">Links</h3>
+                  <div>
+                    <ul className="footer-widget__links-list list-unstyled clearfix">
+                      {links.slice(0, 6).map(({ id, title, href }) => (
+                        <li key={id}>
+                          <Link href={href}>{title}</Link>
+                        </li>
+
+                      ))}
+                      <br />
+                      <li>
+                        <div className="button-header-btn">
+                          <Link href="/Singup-Form" className="footer-widget__contact-text footerenq-btn">Enquiry Now</Link>
+                        </div>
+                      </li>
+                    </ul>
+
+                  </div>
+                  <ul className="footer-widget__links-list footer-widget__links-list-two list-unstyled clearfix">
+                    {links.slice(6).map(({ id, title, href }) => (
+                      <li key={id}>
+                        <Link href={href}>{title}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+              </Col>
+              <Col xl={5} lg={4} className="animated fadeInUp">
+                <div className="footer-widget__column footer-widget__newsletter">
+                  <h3 className="footer-widget__title">Newsletter</h3>
+                  {/* <form
+                    onSubmit={handleSubmit}
+                    className="footer-widget__newsletter-form"
+                  >
+                    <div className="footer-widget__newsletter-input-box">
+                      <input
+                        type="email"
+                        placeholder="Email address"
+                        name="email"
+                      />
+                      <button
+                        type="submit"
+                        className="footer-widget__newsletter-btn"
+                      >
+                        <i className="far fa-paper-plane"></i>
+                      </button>
+                    </div>
+                  </form> */}
+                  <div className="footer-widget__newsletter-bottom">
+                    <div className="footer-widget__newsletter-bottom-icon">
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="footer-widget__newsletter-bottom-text">
+                      <p>I agree to all your terms and policies</p>
+                    </div>
+                  </div>
+                </div>
+                <Row>
+                  <Col xl={12}>
+                    <div className="footer-partners">
+                      <a href="https://maps.app.goo.gl/ryxbajb4yg24TbrG8">
+                        <img src={googlereview.src} alt="googlereview" width="100px" height="40px" />
+                      </a>
+
+                      <img src={googlepartner.src} alt="googlepartner" width="150px" height="55px" />
+                      <a href="https://www.facebook.com/TzarAgency">
+                        <img src={facebook.src} alt="facebook" width="130px" height="50px" />
+                      </a>
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </div>
+          {/* <div className="singup-none">
+            <Link href="/Singup-EnquiryNow" className="footer-widget__contact-text">Enquiry Now</Link>
+          </div> */}
+          <div className="singup-none">
+            <h5 className="footer-widget__titlebranch">Most Popular Location For Our Online Digital Marketing Services: </h5>
+            <Link href="/Digital-Marketing-Services-in-Mumbai" className="footer-widget__contact-text">Digital Marketing Services in Mumbai</Link>
+            {/* &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Chennai" className="footer-widget__contact-text">Digital Marketing Services in Chennai</Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Kolkata" className="footer-widget__contact-text">Digital Marketing Services in Kolkata</Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Hyderabad" className="footer-widget__contact-text">Digital Marketing Services in Hyderabad</Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Delhi" className="footer-widget__contact-text">Digital Marketing Services in Delhi</Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Bangalore" className="footer-widget__contact-text">Digital Marketing Services in Bangalore </Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Gurgaon" className="footer-widget__contact-text">Digital Marketing Services in Gurgaon</Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Ghaziabad" className="footer-widget__contact-text">Digital Marketing Services in Ghaziabad</Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Noida" className="footer-widget__contact-text">Digital Marketing Services in Noida & Greater Noida  </Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Pune" className="footer-widget__contact-text">Digital Marketing Services in Pune </Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Ahmedabad" className="footer-widget__contact-text">Digital Marketing Services in Ahmedabad </Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Lucknow" className="footer-widget__contact-text">Digital Marketing Services in Lucknow </Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Indore" className="footer-widget__contact-text">Digital Marketing Services in Indore </Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-Kanpur" className="footer-widget__contact-text">Digital Marketing Services in Kanpur </Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-USA" className="footer-widget__contact-text">Digital Marketing Services in USA </Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-USA" className="footer-widget__contact-text">Digital Marketing Services in New York </Link>
+            &nbsp; | &nbsp; <Link href="/Digital-Marketing-Services-in-USA" className="footer-widget__contact-text">Digital Marketing Services in New jersey </Link> */}
+          </div>
+        </Container>
+      </div>
+      <div className="site-footer__bottom">
+        <Container>
+          <Row>
+            <Col xl={12}>
+              <div className="site-footer__bottom-inner">
+                <p className="site-footer__bottom-text">
+                  © Copyrights, {year} <a href="#">{author}</a>
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </footer>
+  );
+};
+
+export default SiteFooter;
--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ import "../assets/vendors/animate/animate.min.css";
 import "../assets/vendors/animate/custom-animate.css";
 import "../assets/vendors/reey-font/stylesheet.css";
 import "../assets/vendors/the-sayinistic-font/stylesheet.css";
+import SiteFooter from "./componants/FooterSection/SiteFooter";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +24,90 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+     <head>
+        <meta content="India's fastest growing digital marketing agency | company in India - Tzar Digital Agency Mumbai" property="og:title" />
+        <meta content="https://www.tzar.co/" property="og:url" />
+        <meta name="description" content="Online digital marketing Agency -  Tzar Venture India's fatest growing company/agency in online digital marketing services. DMA is a web marketing agency that offers SEO services, PPC services, social media marketing services, web design services." />
+        <meta name="next-head-count" content="8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="robots" content="index, follow"></meta>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+
+        <script src="https://smtpjs.com/v3/smtp.js"></script>
+        <script async src="//www.instagram.com/embed.js"></script>
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Federo&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+
+
+        <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css" />
+
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.1/mdb.min.css" rel="stylesheet" />
+
+
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,400;1,500;1,700;1,800&display=swap" rel="stylesheet" />
+
+
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.1.0/mdb.min.css"
+          rel="stylesheet"
+        />
+
+
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/9.0.1/swiper-bundle.min.css"
+          rel="stylesheet"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css"
+        />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js"></script>
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charSet="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+
+        <link href=
+          "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          rel="stylesheet" />
+        <script src=
+          "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js">
+        </script>
+
+        <meta name="facebook-domain-verification" content="sog1mzogyompx9km1xruxelfqoqx9b" />
+
+      </head>
+      <body className={inter.className}>
+      {children}
+      
+      <SiteFooter/>
+      </body>
     </html>
   );
 }
